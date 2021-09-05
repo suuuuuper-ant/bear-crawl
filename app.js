2d515ff2a72c5d22d0e00cc7f37de2e8e4c15a7f
ManHyuk <manhyuk@kw.ac.kr>
2021-09-05 16:02:02 +0900

    feat: add struct

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const env = process.env.NODE_ENV || 'develop';
+if (!env) {
+  console.log(`NODE_ENV is ${env}`);
+  console.log('SET NODE_EV!!! development or test or production');
+  process.exit(1);
+}
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => {
+  res.r = (result) => {
+    res.json({
+      isSuccess: true,
+      status: 200,
+      description: '성공',
+      message: 'success',
+      result,
+    });
+  };
+  next();
+});
+
+require('./routes')(app);
+require('./ErrorHandler')(app);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.info(`Listening on Port ${PORT} / at ${env} Env`);
+});
+
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+module.exports = (router) => {
+
+  router.route('/')
+    .get();
+
+
+  return router;
+};
